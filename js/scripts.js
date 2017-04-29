46c93491d210ae36b86328031bfdc77c57274446
Ender187 <m.hiltawski@hotmail.com>
2017-04-29 23:40:15 +0200

    zadanie

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var text: 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var animal = 'Zielone słonie';
+var animalUpperCase = animal.toUpperCase();
+var textChange = text.replace('Papugi', animalUpperCase);
+console.log(text.length/2);
