f7789ad9ce2265376c3a1e9602641b4d7595d6f2
Maciej <m.hiltawski@hotmail.com>
2017-04-30 19:52:12 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
-var text: 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
 var animal = 'Zielone słonie';
 var animalUpperCase = animal.toUpperCase();
 var textChange = text.replace('Papugi', animalUpperCase);
-console.log(text.length/2);
+alert(textChange.slice(0, textChange.length/2));
+console.log(textChange.slice(0, textChange.length/2));
